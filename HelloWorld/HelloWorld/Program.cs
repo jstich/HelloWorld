8b3e797ca8395ac8eee6e7675c32b024245a8d0d
Jacob Stich <jts5blue@gmail.com>
2016-10-25 10:06:35 -0500

    added text

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("holy confusing");
+            Console.WriteLine("test");
         }
     }
 }
