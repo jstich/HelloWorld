ef7168685c1806f59bebf518530b9adf4e933527
Jacob Stich <jts5blue@gmail.com>
2016-10-25 09:14:03 -0500

    Confused AF

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlds");
+            Console.WriteLine("holy confusing");
         }
     }
 }
